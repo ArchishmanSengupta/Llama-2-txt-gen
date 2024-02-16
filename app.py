5c882bed3ea6a96a43528c7656a79ce739761b51
Archishman Sengupta <archishman1808@gmail.com>
2024-02-16 23:48:11 +0530

    feat: add columns for total words and tg

--- app.py
+++ app.py
@@ -10,3 +10,18 @@ st.set_page_config(
   initial_sidebar_state='collapsed',
 )
 
+st.header("Generate Topic Summary")
+
+input_text = st.text_input("Enter the topic")
+
+## create columns for number of words and target audience
+
+col1,col2 = st.columns([5,5])
+
+with col1:
+  numberOfWords = st.text_input("Number of words")
+with col2:
+  targetAudience = st.selectbox("Writing the topic for", ('students', 'professionals', 'researchers'), index = 0)
+
+submit = st.button("Generate")
+
